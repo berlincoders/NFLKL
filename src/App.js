34b4a91c10495b5b09318a3c2c4df40e91517b86
Romeo <ralonsoad@gmail.com>
2024-03-22 09:21:24 +0100

    Install React Player

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
 
 const App = () => {
 
   return (
     <div className="App">
       <h1>Hello React!</h1>
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=GU1o2blfeO0"
+      />
     </div>
 
   );
