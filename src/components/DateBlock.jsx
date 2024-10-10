c2e9774dfcc5b5d19ee187f71d573c6db73d16e0
Romeo <ralonsoad@gmail.com>
2024-10-10 14:08:34 +0200

    Adjust card size

--- src/components/DateBlock.jsx
+++ src/components/DateBlock.jsx
@@ -5,30 +5,24 @@ import DateDisplay from './DateDisplay';
 
 const DateBlock = ({ dates }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const itemsPerPage = 3; // Number of days to display
+  const itemsPerPage = 3;
 
-  // Debugging: Log the received props
   useEffect(() => {
     console.log("DateBlock props:", { dates });
   }, [dates]);
 
-  // Get the current set of dates to display
   const limitedDates = dates.slice(currentIndex, currentIndex + itemsPerPage);
 
   return (
-    <div className="mb-5 w-full">
-      {/* Iterate over each date and its corresponding games */}
+    <div className="mb-1 w-full">
       {limitedDates.map((dateObj, index) => (
-        <div key={index} className="mb-5 w-full flex">
-          {/* DateDisplay at the start of each row */}
+        <div key={index} className="w-full flex">
           <div className="flex-shrink-0 w-[150px] text-center">
             <DateDisplay date={dateObj.date} />
           </div>
-
-          {/* Games for the current date */}
-          <div className="flex-1 flex flex-wrap justify-start">
+          <div className="flex-1 flex flex-wrap justify-start items-start"> {/* Ensure no height restrictions */}
             {dateObj.games.map((game, gameIndex) => (
-              <div key={gameIndex} className="p-2 w-full sm:w-1/2 lg:w-1/3">
+              <div key={gameIndex} className="sm:w-1/2 lg:w-1/3">
                 {game.available ? (
                   <GameCard game={game} />
                 ) : (
@@ -44,4 +38,3 @@ const DateBlock = ({ dates }) => {
 };
 
 export default DateBlock;
-
